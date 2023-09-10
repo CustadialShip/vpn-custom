c44b00e2e7e92ad5ba0a1a4c3ed579587fc1ae55
Dmitriy Sharandikov <custadialship@gmail.com>
2023-09-10 19:39:28 +0300

    Clean up front code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,12 +4,9 @@ import store from './store/store';
 import Navbar from './components/Navbar';
 import Home from "./components/Home";
 import ShowNavbar from "./components/ShowNavbar";
-import Create from "./components/Create";
-import BlogDetails from "./components/BlogDetails";
 import NotFound from "./components/NotFound";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Profile from "./components/Profile";
 
 
 function App() {
@@ -33,15 +30,6 @@ function App() {
                             </Route>
                             <Route exact path="/home">
                                 <Home/>
-                            </Route>
-                            <Route path="/create">
-                                <Create/>
-                            </Route>
-                            <Route path="/blogs/:id">
-                                <BlogDetails/>
-                            </Route>
-                            <Route path="/profile">
-                                <Profile/>
                             </Route>
                             <Route path="*">
                                 <NotFound/>
--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,37 +1,9 @@
-import useFetch from "../hooks/useFetch";
-import BlogList from "./BlogList";
-import {useEffect, useState} from "react";
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-
 const Home = () => {
-
-    const [currentPage, setCurrentPage] = useState(1);
-    const {data: responseBlogs, isPending, error} = useFetch('/api/v1/blogs?page=' + currentPage);
-    const [pageNumbers, setPageNumbers] = useState(null);
-
-    useEffect(() => {
-        if (responseBlogs && responseBlogs.totalPage) {
-            setPageNumbers([...Array(responseBlogs.totalPage).keys()].slice(1));
-        }
-    }, [responseBlogs]);
-
-    const handleChange = (event, value) => {
-        setCurrentPage(value);
-    };
 
     return (
         <div className="home">
-            {error && <div>{error}</div>}
-            {isPending && <div>Loading...</div>}
-            {responseBlogs && responseBlogs.blogs && <BlogList blogs={responseBlogs.blogs}/>}
-            <div className="blogPagination">
-                <Stack spacing={2}>
-                    {pageNumbers && <Pagination count={pageNumbers.length + 1}
-                                                page={currentPage}
-                                                onChange={handleChange}
-                                                defaultPage={0}/>}
-                </Stack>
+            <div>
+                Home
             </div>
         </div>
     );
